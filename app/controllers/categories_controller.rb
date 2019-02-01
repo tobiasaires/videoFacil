671df6a52f51e74b65f69a8a80cd7a1f93ec965e
Tobias Aires <tobiasaires@gmail.com>
2019-01-31 22:14:14 -0300

    CRUD actions in Category model

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,56 @@
+class CategoriesController < ApplicationController
+
+    before_action :find_category, only: [:show, :edit, :update, :destroy]
+
+
+    def index
+        @categories = Category.all.where(is_favorite: true)
+    end
+
+    def new
+        @category = Category.new
+    end
+    
+    def create
+        @category = Category.new(category_params)
+        if @category.save()
+            redirect_to root_path
+        else 
+            render 'new'
+        end
+    end
+    
+    def show   
+    end
+    
+    def edit
+    end
+
+    def update
+        if @category.update(category_params)
+            redirect_to category_path
+        else 
+            redirect_to 'edit'
+        end
+    end
+
+    def destroy
+        @category.destroy
+        redirect_to root_path
+    end
+
+    def all
+        @categories = Category.all
+    end
+
+
+    private
+        
+        def category_params
+            params.require(:category).permit(:name, :is_favorite)
+        end
+
+        def find_category
+            @category = Category.find(params[:id])
+        end
+end
