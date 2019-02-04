5271ac296d9ca1dcfa604074895b4f4d4d437000
Tobias Aires <tobiasaires@gmail.com>
2019-02-04 10:29:16 -0300

    resolving destroy associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-    has_many :videos
+    has_many :videos, dependent: :destroy
     validates :name, presence: true
     validates :is_favorite, presence: true
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ApplicationRecord
   belongs_to :category
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :url, presence: true
